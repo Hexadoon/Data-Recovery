5e86b01ec3fb6743b5bf6f53313700f33165ad6e
Akash Pathuri <akash.pathuri@gmail.com>
2020-06-24 14:41:32 -0400

    added player Animation and revised game mechanics

--- Data-Recovery/Assets/Scripts/Enemy Scripts/peon.cs
+++ Data-Recovery/Assets/Scripts/Enemy Scripts/peon.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class peon : enemyBehavior{
 
-    //public float travelSpeed;
     bool right = true;
     private float timeBtwShots;
-    //public float health = 50;
     public float maxDistance;
     float minDistance;
     float currentDistance;
@@ -15,18 +13,19 @@ public class peon : enemyBehavior{
 
     void Start(){
         Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>().transform;
+        playerProfile = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+
         health = 50;
         minDistance = gameObject.transform.position.x;
         maxDistance = maxDistance + minDistance;
         gameObject.GetComponent<Rigidbody2D>().velocity = transform.right * travelSpeed;
-        timeBtwShots = startTimeBtwShots;
-        //fireRate = 5f;
-        //nextFire = Time.time;
-
+        timeBtwShots = 0;
     }
 
     void Update(){
-        float distance = Vector3.Distance(transform.position, Player.position);
+        float distance = Mathf.Abs(transform.position.x - Player.position.x);
+        float ydistance = Mathf.Abs(transform.position.y - Player.position.y);
+
 
         currentDistance = gameObject.transform.position.x;
         if (currentDistance >= maxDistance && right == true)
@@ -38,21 +37,9 @@ public class peon : enemyBehavior{
             flip();
         }
 
-        if (distance >= MaxDist) {
-          gameObject.GetComponent<Rigidbody2D>().velocity = transform.right * travelSpeed;
 
-        }
-
-        // if player is within first range, chase after player
-
-        else if (distance >= MinDist && distance <= MaxDist) {
-            //transform.LookAt(Player);
-            //transform.rotation =  Quaternion.LookRotation((Player.position - transform.position).normalized , Vector3.up);
-            //transform.LookAt(Player.transform.position);
-            //transform.Rotate(transform.right);
-            //transform.position = Vector2.MoveTowards(transform.position, Player.position, 1 * Time.deltaTime);
-
-            //gameObject.transform.localEulerAngles = new Vector3(0f, 90f, 0f);
+        if (distance <= MaxDist  && ydistance<=1) {
+            
             if (Player.position.x < transform.position.x) {
               transform.localEulerAngles = new Vector3 (0f, 180f, 0f);
             } else {
@@ -61,14 +48,15 @@ public class peon : enemyBehavior{
             }
             gameObject.GetComponent<Rigidbody2D>().velocity = transform.right * 0;
             transform.position = this.transform.position;
+            if (distance <= MinDist)
+            {
+                CheckIfTimeToFire();
+
+            }
         }
-
-
-        // if player is within second range, shoot the player
-        if (distance <= MinDist) {
-          //transform.LookAt(Player);
-          CheckIfTimeToFire();
-
+        else
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = transform.right * travelSpeed;
         }
     }
 
@@ -88,38 +76,23 @@ public class peon : enemyBehavior{
         }
     }
 
-/**
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "Respawn")
-        {
-            flip();
-        }
-        if(collision.tag == "Bullet" && collision.gameObject.layer != 8)
-        {
-            Debug.Log(collision.gameObject.layer);
-            Debug.Log("Bullet Hit");
-            bulletMachanics projectileScript = collision.GetComponent<bulletMachanics>();
-            float damageOccured = projectileScript.damage;
-            health -= damageOccured;
-
-            if (health <= 0)
-            {
-                Destroy(gameObject);
-            }
-        }
-    }
-**/
-
     void CheckIfTimeToFire() {
-      if (timeBtwShots <= 0) {
-        bulletObject.layer = 8;
-        Instantiate(bulletObject, spawnLocation.position, spawnLocation.rotation);
-        timeBtwShots = startTimeBtwShots;
-        //nextFire = Time.time + fireRate;
-      }
+      if (timeBtwShots <= 0 && playerProfile.playerHealth>0) {
+            bulletObject.layer = 8;
+            Instantiate(bulletObject, spawnLocation.position, spawnLocation.rotation);
+            timeBtwShots = startTimeBtwShots;
+            if (playerProfile.playerHealth <= 0)
+            {
+                Debug.Log("killed"); 
+                timeBtwShots = startTimeBtwShots+2f;
+            }
+
+        }
       else {
-        timeBtwShots -= Time.deltaTime;
+            if (playerProfile.playerHealth > 0)
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
       }
     }
 }
